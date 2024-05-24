cea9de63fee6bcb080ec2f9498c49c3b28266cf5
Rainbowizard67 <goboysgo42@gmail.com>
2024-05-23 23:28:23 -0400

    I shid myself

--- server.c
+++ server.c
@@ -2,12 +2,20 @@
 #include<stdlib.h>
 #include<string.h>
 #include<sys/socket.h>
-
+#include<sys/types.h>
+#include<sys/un.h>
+#include<stdbool.h>
 
 
 int main() {
     
-    
-    
+    while(true) {
+        
+        int endpoint = socket(AF_INET, SOCK_STREAM, 0);
+
+        
+
+    }
+
     return 0;
 }
