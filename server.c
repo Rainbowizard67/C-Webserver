bc3b9d5a8eef7681f8598d6fffcb1e6b8d7c8134
Rainbowizard67 <goboysgo42@gmail.com>
2024-05-24 01:38:41 -0400

    Update to the server

--- server.c
+++ server.c
@@ -4,16 +4,35 @@
 #include<sys/socket.h>
 #include<sys/un.h>
 #include<stdbool.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
 
+#define backlog 50
 
 int main() {
     
+    int socIpv4 = socket(AF_INET, SOCK_STREAM, 0); //creates the socket, socketfd = socket(domain, type, protocol);
+    if (socIpv4==-1) {
+        EXIT_FAILURE;
+    }
+
+    
+
+    if(bind(socIpv4, , ) == -1) {
+
+    }
+
+
+
+
+
     while(true) {
-        int endpoint = socket(AF_INET, SOCK_STREAM, 0);
 
-        int endpoint = socket(AF_INET6, SOCK_STREAM, 0);
-
-        listen(endpoint, 100);
+        if (listen(socIpv4, backlog) == -1) {
+            close(socIpv4);
+            EXIT_FAILURE;
+        } //listens for client socket, listen(socketfd, backlog);
 
     }
 
