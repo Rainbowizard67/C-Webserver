ca5ab603e17350ffcf90ec5a08ff20517d556bec
Rainbowizard67 <goboysgo42@gmail.com>
2024-05-24 00:10:23 -0400

    Continuing the webserver

--- server.c
+++ server.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 #include<sys/socket.h>
-#include<sys/types.h>
 #include<sys/un.h>
 #include<stdbool.h>
 
@@ -10,10 +9,11 @@
 int main() {
     
     while(true) {
-        
         int endpoint = socket(AF_INET, SOCK_STREAM, 0);
 
-        
+        int endpoint = socket(AF_INET6, SOCK_STREAM, 0);
+
+        listen(endpoint, 100);
 
     }
 
