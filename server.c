e154d6ef366ce321b5ca3528bc93ba38f37fe7cc
root <root@AlexRob67>
2024-08-16 17:46:33 -0400

    New handle client file and update to main server file

--- server.c
+++ server.c
@@ -1,39 +1,26 @@
 #include"server.h"
 
-/* struct sockaddr_in {
-    short int sin_family;   //Address Family (Ex: AF_INET)
-    unsigned short int sin_port;    //Port number
-    struct in_addr sin_addr;    //IPv4 address
-    unsigned char sin_zero[8];  //Padding (zero), you do not need to set this
-   };
-*/
-
-/*
-    struct in_addr {
-        unsigned long s_addr; //32-bit Ipv4 address in Network order
-    };
-*/
 
 int main(int argc, char *argv[]) {
-    struct sockaddr_in serverAddress; //sets the variable to "serverAddress" to the socket.h data type sockaddr_in, above is what the struct looks like
+    struct sockaddr_in serverAddress; //structure for server socket interface
 
     socklen_t addrlen = sizeof(serverAddress); //socklen_t=unsigned int, size of the serverAddress struct
 
-    int clientSocket;
+    int socClient; //client socket
 
-    int ipAdd = ""; //local Ipv4 address used to bind for the server/socket
+    int ipAdd = ""; //local Ipv4 address used to bind for the server socket
 
-    int socIpv4 = socket(AF_INET, SOCK_STREAM, 0); //creates the socket, socketfd = socket(domain, type, protocol);
-
-    if (socIpv4==-1) {
+    int socIpv4 = socket(AF_INET, SOCK_STREAM, 0); //server socket
+    //socket(int domain, int type, int protocol)
+        
+    if (socIpv4 == -1) {
+        close(socIpv4);
         exit(EXIT_FAILURE);
-    }
+    } //creates the socket, socketfd = socket(domain, type, protocol);
 
-    //next couple lines set the members of the serverAddress struct/general address structure
+    //struct setup
     memset(&serverAddress, 0, addrlen); //sets the struct in memory all to the value of zero
-
-    serverAddress.sin_family = AF_INET; //sets the address family
-    
+    serverAddress.sin_family = AF_INET; //sets the address family    
     serverAddress.sin_port = htons(PORT); //htons converts the int to network byte order (from the 8080 to the actual port)
     
     if (inet_pton(AF_INET, ipAdd, &serverAddress.sin_addr) <= 0) { //converts the text rep of Ipv4 address to the binary form, it is then stored in the serverAddress struct
@@ -41,7 +28,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     } //inet_pton(addressFamily, src, networkAddrStruct);
 
-    if(bind(socIpv4, serverAddress.sin_addr.s_addr, addrlen) == -1) { //binds the ip address to the socket so that they are synonymous
+    if  (bind(socIpv4, serverAddress.sin_addr.s_addr, addrlen) == -1) { //binds the ip address to the socket so that they are synonymous
         close(socIpv4);
         exit(EXIT_FAILURE);
     } //bind(socketfd, addr, addrlen);
@@ -57,7 +44,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
         } //listen(socketfd, backlog);        
         
-        if ((clientSocket = accept(socIpv4, &serverAddress.sin_addr.s_addr, &addrlen)) < 0) { //accepts clients to the server from the listen backlog connection request list
+        if ((socClient = accept(socIpv4, &serverAddress.sin_addr.s_addr, &addrlen)) < 0) { //accepts clients to the server from the listen backlog connection request list
             perror("No clients dectected yet\n");
             continue;
         } //accept(socketfd, addr, addrlen);
--- server.h
+++ server.h
@@ -1,10 +1,10 @@
 #ifndef PREPROCESS_NETWORK
 #define PREPROCESS_NETWORK
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
+#include<stdio.h> //standard I/O ouput 
+#include<stdlib.h> //standard C library
+#include<stdbool.h> //standard boolean library
+#include<string.h> //standard string library
 #include<errno.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -15,7 +15,22 @@
 
 // Macros and global variables
 #define backlog 10
-#define PORT 8080
+#define PORT 8080 //port number
 #define buffer 1024 //exactly one KB
 
 #endif
+
+//Structs
+
+/* struct sockaddr_in {
+    short int sin_family;   //Address Family (Ex: AF_INET)
+    unsigned short int sin_port;    //Port number
+    struct in_addr sin_addr;    //IPv4 address
+    unsigned char sin_zero[8];  //Padding
+   };
+*/
+
+/*  struct in_addr {
+        unsigned long s_addr; //32-bit Ipv4 address in Network order
+    };
+*/
