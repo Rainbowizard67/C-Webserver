fd3bec0feb821f246407f3ac0f97d883b0c80e0e
root <root@alexRMain>
2025-01-13 02:07:54 -0500

    added project-header file

---
+++ src/headers/project_headers.h
@@ -0,0 +1,18 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <stdio.h> //standard I/O ouput
+#include <stdlib.h> //standard C library
+#include <stdbool.h> //standard boolean library
+#include <string.h> //standard string library
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <unistd.h> //standard Unix OS library
+#include <pthread.h> //standard C thread library
+#include <ncurses.h> //text based user interfaces
+
+#endif
