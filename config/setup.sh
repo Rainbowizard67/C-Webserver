6ff7bce7cc20932331921159ada7a97d1ad2a087
Rainbowizard67 <goboysgo42@gmail.com>
2025-06-04 11:47:16 -0400

    Added and updated setup script for program depends

--- config/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is for install C libraries needed, this script is referenced in the make project script in this directory
-
-apt install libcyaml-dev -y
---
+++ config/setup_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install valgrind gdb build-essential make libyaml-dev libcyaml-dev -y
+
