e944894aec5b127d5e7610f6d1770d6a6f19563f
Rainbowizard67 <goboysgo42@gmail.com>
2025-10-06 14:21:27 -0400

    Adding new cpp and hpp file variants

---
+++ src/include/handleClient.hpp
@@ -0,0 +1,26 @@
+#ifndef PREPROCESS_HANDLECLIENT_HPP
+#define PREPROCESS_HANDLECLIENT_HPP
+
+#include "project_headers.h"
+#include "fileLoader.h"
+#include "cache.h"
+
+#define MAX_CACHE_SIZE 5
+#define MAX_METHOD_SIZE 10
+
+namespace clientHandler {
+    struct timeval timeout;
+    char* d_web_path;
+    char* d_config_path;
+
+    typedef struct parse_data {
+        bool check;
+        char method[MAX_METHOD_SIZE];
+    }parse_data_t;
+
+
+}
+
+void http_client_handler(client_connection_t* client);
+
+#endif
