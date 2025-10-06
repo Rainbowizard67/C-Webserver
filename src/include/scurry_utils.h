92b2413f41755aebfedfe2a2de7d7fe410643daf
Rainbowizard67 <goboysgo42@gmail.com>
2025-10-06 15:57:02 -0400

    Consilidating files and moving things around

---
+++ src/include/scurry_utils.h
@@ -0,0 +1,15 @@
+#ifdef SCURRY_UTILS_H
+#define SCURRY_UTILS_H
+
+// Start include list of library dependencies
+#include "project_headers.h"
+// End include list of library dependencies
+
+/*===================
+|                   |
+|                   |
+|                   |
+====================*/
+
+
+#endif
