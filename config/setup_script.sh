08b6532d97fdeadb43e1452953abf6a8c908e2b5
Rainbowizard67 <goboysgo42@gmail.com>
2025-10-06 14:19:29 -0400

    Transferring C to C++ files and changing env script portion

--- config/setup_script.sh
+++ config/setup_script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+###Start Function declaration###
+
+find_config() {
+    local env_file="$1"
+    export SCURRY_CONFIG_DIR="$(pwd)"
+    echo
+}
+
+###End Function Declaration###
+
 sudo printf "%b\n" "\e[0;34mDownloading and Installing dependencies...\e[0m"
 
 sudo apt update
@@ -7,10 +17,16 @@ sudo apt install valgrind gdb build-essential libyaml-dev libcyaml-dev -y
 
 sudo printf "%b\n" "\e[0;34mFinished dependency install\e[0m"
 
+sudo printf "%b\n" "\e[0;34mStarting env file initialization...\e[0m"
+
 ENV_FILE="scurry.env"
 
 cat > "$ENV_FILE" <<EOL
-SCURRY_CONFIG_DIR
+###Scurry Env###
 
 EOL
+
+find_config "$ENV_FILE"
+
+sudo printf "%b\n" "\e[0;34mFinished env file initialization\e[0m"
 
